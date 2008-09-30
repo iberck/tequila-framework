f7c943387c5e01536928e4d11dbe010014e0207c
iberck <iberck@130e9264-805b-11dd-a170-e1db062c8548>
2008-09-30 07:26:29 +0000

    Added basic project structure

--- trunk/Tequila-framework/src/main/java/org/tequila/project/InternalClassPath.java
+++ trunk/Tequila-framework/src/main/java/org/tequila/project/InternalClassPath.java
@@ -21,8 +21,6 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
