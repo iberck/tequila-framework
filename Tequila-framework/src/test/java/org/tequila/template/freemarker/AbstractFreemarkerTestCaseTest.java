65348d3ecd9278f82c67782c2e16dffca0cdeff7
iberck <iberck@130e9264-805b-11dd-a170-e1db062c8548>
2008-10-01 06:59:03 +0000

    AbstractFreemarkerTestCase

---
+++ Tequila-framework/src/test/java/org/tequila/template/freemarker/AbstractFreemarkerTestCaseTest.java
@@ -0,0 +1,50 @@
+package org.tequila.template.freemarker;
+
+import java.io.File;
+import java.util.Map;
+import org.tequila.project.JProject;
+import org.tequila.project.NbJProject;
+
+/**
+ *
+ * @author iberck
+ */
+public class AbstractFreemarkerTestCaseTest extends AbstractFreemarkerTestCase {
+
+    public AbstractFreemarkerTestCaseTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Testea el testcase de freemarker
+     */
+    public void testAbstractFreemarkerTestCase() throws Exception {
+        // un proyecto cualquiera
+        JProject nbProject = new NbJProject("./src/test/resources/NbApplication");
+        nbProject.setup();
+        Map projectWrap = nbProject.wrap();
+
+        String templateDef = "${project.name}, ${project.path}, ${project.srcPath}," +
+                "${project.classesPath}, ${project.testPath}";
+
+        String expected = "NbApplication, ./src/test/resources/NbApplication, src," +
+                "build" + File.separator + "classes, test";
+
+        // test case de las cadenas.
+        assertEqualsFreemarkerTemplate(projectWrap, templateDef, expected);
+
+        // test case de los archivos.
+        File temFile = new File("./src/test/resources/templates/test1.ftl");
+        assertEqualsFreemarkerTemplate(projectWrap, temFile, "src");
+    }
+}
